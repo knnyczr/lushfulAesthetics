72e59d29495e6eceb2fe34822017fac6a33f8dce
Kenny <kenny@knnyczr.com>
2022-08-26 23:50:52 -0400

    reading images and raw text from contentful

--- gatsby-node.js
+++ gatsby-node.js
@@ -21,40 +21,8 @@ exports.createPages = async ({ graphql, actions }) => {
       path: `/${edge.node.slug}`,
       component: servicePageTemplate,
       context: {
-        data: edge.node.slug,
+        servicePageSlug: edge.node.slug,
       },
     });
   });
-
-  //   return new Promise((resolve, reject) => {
-  //     resolve(
-  //       graphql(`
-  // {
-  //   allContentfulServicePage {
-  //     edges {
-  //       node {
-  //         slug
-  //       }
-  //     }
-  //   }
-  // }
-  //       `)
-  //     ).then((result) => {
-  //       if (result.errors) {
-  //         console.log(result.errors);
-  //         reject(result.errors);
-  //       }
-  //       console.log("HERE IS RESULT: ", result);
-
-  //       result.data.allContentfulServicePage.edges.map((data) => {
-  //         createPage({
-  //           path: `/${data.node.slug}`,
-  //           component: servicePageTemplate,
-  //           context: {
-  //             slug: data.node.slug,
-  //           },
-  //         });
-  //       });
-  //     });
-  //   });
 };
--- src/templates/servicePage.js
+++ src/templates/servicePage.js
@@ -1,40 +1,68 @@
 import React from "react";
 import { graphql } from "gatsby";
+import { useState } from "react";
+import { GatsbyImage } from "gatsby-plugin-image";
+import { useContentfulImage } from "gatsby-source-contentful/hooks";
+import { renderRichText } from "gatsby-source-contentful/rich-text";
 
-export default function ServicePage(data) {
-  console.log("here is data from servicePage: ", data);
-  return <div>hello world</div>;
+export default function ServicePage({ data }) {
+  const [pageData, setPageData] = useState(
+    data.allContentfulServicePage.edges[0].node
+  );
+  console.log("here is page data: ", pageData);
+
+  const dynamicImage = useContentfulImage({
+    image: {
+      url: pageData.heroImage.gatsbyImageData.images.sources[0].srcSet,
+      width: 2000,
+      height: 1000,
+    },
+  });
+
+  return (
+    <div>
+      <GatsbyImage image={dynamicImage} alt="hero image is decorative" />
+      <h1>{pageData.serviceTitle}</h1>
+      <p>{renderRichText(pageData.faq)}</p>
+    </div>
+  );
 }
 
-// export const query = graphql`
-//   query contentfulServicePageQuery {
-//     allContentfulServicePage(sort: { order: ASC }) {
-//       edges {
-//         node {
-//           contentful_id
-//           faq {
-//             raw
-//           }
-//           heroImage {
-//             id
-//           }
-//           howDoesItWork {
-//             raw
-//           }
-//           postCare {
-//             raw
-//           }
-//           pricing {
-//             raw
-//           }
-//           preCare {
-//             raw
-//           }
-//           whatsItFor {
-//             raw
-//           }
-//         }
-//       }
-//     }
-//   }
-// `;
+export const pageQuery = graphql`
+  query servicePageQuery($servicePageSlug: String!) {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+    allContentfulServicePage(
+      filter: { node_locale: { eq: "en-US" }, slug: { eq: $servicePageSlug } }
+      limit: 1
+    ) {
+      edges {
+        node {
+          slug
+          heroImage {
+            gatsbyImageData(layout: FULL_WIDTH)
+          }
+          faq {
+            raw
+          }
+          ourApproach {
+            raw
+          }
+          postCare {
+            raw
+          }
+          preCare {
+            raw
+          }
+          pricing {
+            raw
+          }
+          serviceTitle
+        }
+      }
+    }
+  }
+`;
