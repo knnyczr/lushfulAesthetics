c2d3eaf74b396b4ed40bbac3e9ec02c3469a220a
Kenny <kenny@knnyczr.com>
2023-01-28 17:27:42 -0500

    error with file name capitalization

--- src/components/blog/Categories.js
+++ src/components/blog/Categories.js
@@ -1,5 +1,5 @@
 import React from "react";
-import CategoryChip from "./categoryChip";
+import CategoryChip from "./CategoryChip";
 
 export default function Categories({ categories }) {
   return (
