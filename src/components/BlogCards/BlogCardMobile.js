138a710ea0f1f2277c354eab830c3019d658eebc
Yusong Shi <jackshi.sys@gmail.com>
2023-01-31 11:52:52 -0500

    TW module added, mobil styling added, code clean up

--- src/components/BlogCards/BlogCardMobile.js
+++ src/components/BlogCards/BlogCardMobile.js
@@ -28,9 +28,9 @@ export default function BlogCardMobile({
 const CategoryCards = ({ category, formatDate }) => {
   return (
     <div>
-      <div className="w-full h-auto">
-        <div className="flex overflow-x-scroll">
-          <div className="flex flex-nowrap snap-mandatory snap-x">
+      <div className="w-full h-auto pb-16">
+        <div className="flex overflow-x-scroll ">
+          <div className="flex flex-nowrap snap-mandatory snap-x gap-8">
             {category.map((card, idx) => {
               return (
                 <div
@@ -41,24 +41,22 @@ const CategoryCards = ({ category, formatDate }) => {
                     backgroundRepeat: `no-repeat`,
                     backgroundPosition: `50% 50%`,
                   }}
-                  className="bg-green-400 w-full h-[35rem]"
+                  className="w-blogcard h-[32rem] sm:w-blogcard-sm sm:h-[35rem] relative snap-start"
                 >
-                  <div className="text-white">
-                    <h3>{card[0].title}</h3>
-                    <p>{card[0].intro}</p>
-                    <p>Posted on {formatDate(card[0].datePosted)}</p>
+                  <div className="text-white w-blogcard h-[32rem] sm:w-blogcard-sm sm:h-[35rem] bg-gradient-to-b from-black/0 via-black/0 to-black/75 z-10 cursor-pointer flex flex-col justify-end items-start px-4 py-6">
+                    <h3 className="font-serif text-3xl font-bold">
+                      {card[0].title}
+                    </h3>
+                    <p className="text-sm lg:text-lg my-3 lg:my-4">
+                      Posted on {formatDate(card[0].datePosted)}
+                    </p>
+                    <p className="text-sm">{card[0].intro}</p>
                   </div>
                 </div>
               );
             })}
           </div>
         </div>
-      </div>
-      <div>
-        <h1>Body Aesthetic</h1>
-      </div>
-      <div>
-        <h1>Sexual Enhancement</h1>
       </div>
     </div>
   );
