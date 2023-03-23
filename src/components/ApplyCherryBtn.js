3b99713c974017ab14636388d04c9a151c20e867
Kenny <kenny@knnyczr.com>
2023-03-22 21:07:01 -0400

    quick patch fixing a tag href

--- src/components/ApplyCherryBtn.js
+++ src/components/ApplyCherryBtn.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function ApplyCherryBtn({ url }) {
   return (
     <>
-      <a target="_blank" to={`${url}`}>
+      <a target="_blank" href={`${url}`}>
         <button
           className="py-3 px-6 bg-main-green hover:bg-main-green-shade rounded text-white text-sm lg:text-lg uppercase"
           type="button"
