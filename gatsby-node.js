8d4e8c896699f6666bfc049cb85825ee732ff34a
Kenny <kenny@knnyczr.com>
2024-01-07 16:31:47 -0500

    LBA-8 condensing models, deconstructing allContentfulFooterContent in gatsby node

--- gatsby-node.js
+++ gatsby-node.js
@@ -17,10 +17,9 @@ exports.createPages = async ({ graphql, actions }) => {
     data: {
       allContentfulServicePage,
       allContentfulGeneralFaqPage,
-      allContentfulPrivacyPolicyPage,
-      allContentfulHipaaPolicyPage,
       allContentfulBlogCategory,
       allContentfulBlogPost,
+      allContentfulFooterContent,
     },
   } = await graphql(`
     {
