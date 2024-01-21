0e41497d7cbebb36cf2a3276bc6da6cb47b57186
Yusong Shi <jackshi.sys@gmail.com>
2024-01-21 12:03:23 -0500

    LBA-21 Add PromoItem field, multiple promoitems, field for unbounce link

---
+++ src/components/PromoBanner.js
@@ -0,0 +1,50 @@
+import * as React from "react";
+import { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faAngleLeft, faAngleRight } from "@fortawesome/free-solid-svg-icons";
+
+const promotions = [
+  {
+    text: "Book by 12/25 to get 20% OFF. CODE: DADDY25",
+    link: "/link1",
+  },
+  { text: "2X points on 1/20-1/24", link: "/link1" },
+  { text: "Refer your friend and get 20% OFF", link: "/link1" },
+];
+
+export default function PromoBanner() {
+  const [currentPromotion, setCurrentPromotion] = useState(0);
+
+  const handleNavClick = (direction) => {
+    if (direction === "left") {
+      setCurrentPromotion(
+        (currentPromotion - 1 + promotions.length) % promotions.length
+      );
+    } else {
+      setCurrentPromotion((currentPromotion + 1) % promotions.length);
+    }
+  };
+
+  return (
+    <>
+      <div className="flex justify-center items-center mx-auto bg-main-green py-3 md:py-4 text-sm px-4 md:px-12 lg:px-4">
+        <div className="flex justify-between items-center w-[600px] mx-auto">
+          <button className="" onClick={() => handleNavClick("left")}>
+            <FontAwesomeIcon icon={faAngleLeft} />
+          </button>
+          <div className="">
+            <p>
+              <span className="">{promotions[currentPromotion].text}. </span>
+              <a href={promotions[currentPromotion].link} className="">
+                <span className="underline underline-offset-2">View all</span>
+              </a>
+            </p>
+          </div>
+          <button className="" onClick={() => handleNavClick("right")}>
+            <FontAwesomeIcon icon={faAngleRight} />
+          </button>
+        </div>
+      </div>
+    </>
+  );
+}
