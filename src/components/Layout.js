78f4d1ca9be2de2177b1228f8f267be92341622a
Kenneth Cruz <37004078+knnyczr@users.noreply.github.com>
2024-01-22 19:28:03 -0500

    LBA-46 trade pixel bug and moving other tracking to conditional rendering (#102)

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -15,12 +15,31 @@ export default function Layout({ children }) {
 export function Head({ children }) {
   return (
     <>
-      <meta
-        name="facebook-domain-verification"
-        content="lih9j34woa8ztip98myvop39w9zcy6"
-      />
-
       <meta httpEquiv="content-language" content="en-us" />
+      {process.env.NODE_ENV === "production" && (
+        <>
+          <meta
+            name="facebook-domain-verification"
+            content="lih9j34woa8ztip98myvop39w9zcy6"
+          />
+
+          <script
+            src="https://js.adsrvr.org/up_loader.1.1.0.js"
+            type="text/javascript"
+          />
+          <script type="text/javascript">
+            {`
+              ttd_dom_ready( 
+                function() { 
+                  if (typeof TTDUniversalPixelApi === 'function') { 
+                    var universalPixelApi = new TTDUniversalPixelApi(); universalPixelApi.init("t2f05ni", ["yekzz77"], "https://insight.adsrvr.org/track/up");
+                  } 
+                }
+              )
+            `}
+          </script>
+        </>
+      )}
       {children}
     </>
   );
