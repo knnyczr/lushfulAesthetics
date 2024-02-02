cca1cb1a634bf1437d44f2fa0c11744803e75e57
Kenny <kenny@knnyczr.com>
2024-02-01 22:04:54 -0500

    LBA-18 Data Disclosure Query

--- src/components/Context.js
+++ src/components/Context.js
@@ -3,6 +3,7 @@ import React, { createContext } from "react";
 export const initialState = {
   name: "",
   email: "",
+  hasSeenDataDisclosurePopover: false,
 };
 
 export const Context = createContext(initialState);
