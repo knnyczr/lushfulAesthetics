1a235ac23b6ca6331a178e4034dc4611015aab09
Yusong Shi <jackshi.sys@gmail.com>
2024-02-28 11:39:34 -0500

    LBA-57-moved the labels and added background for better contrast

--- src/components/beforeAndAfter/ImageModal.js
+++ src/components/beforeAndAfter/ImageModal.js
@@ -37,19 +37,14 @@ export default function ImageModal({
     },
   });
 
-  // These 2 lines are for infinite loop toggels for previous and next
-  // const showPrevButton = currentIndex > 0;
-  // const showNextButton = currentIndex < imagePairs.length - 1;
-
   return (
     <div className="fixed inset-0 bg-black bg-opacity-80 flex justify-center items-center p-4 z-50">
-      <div className="p-4 rounded flex overflow-auto max-w-full max-h-full">
+      <div className="p-4 rounded flex flex-col justify-center overflow-auto max-w-full max-h-full">
         <div className="flex w-full max-w-4xl">
           {/* Previous Button */}
-          {/* {showPrevButton && ( */}
           <button
             onClick={onPrev}
-            className="absolute left-0 top-1/2 z-10 px-4 md:px-12 lg:px-4 text-white text-3xl"
+            className="absolute left-0 top-[45%] z-10 px-4 md:px-12 lg:px-4 text-white text-3xl"
             aria-label="Previous image"
           >
             <FontAwesomeIcon
@@ -57,7 +52,6 @@ export default function ImageModal({
               icon={faArrowLeft}
             />
           </button>
-          {/* )} */}
           {/* Before Image */}
           <div className="w-1/2 relative">
             <GatsbyImage
@@ -65,9 +59,6 @@ export default function ImageModal({
               className="w-full h-full object-cover aspect-[3/4]"
               alt={currentImagePair.before.title}
             />
-            <div className="absolute w-full h-full top-0 left-0 flex items-end justify-end text-white text-lg font-bold pr-6 pb-6 bg-gradient-to-t from-black/50 to-transparent">
-              BEFORE
-            </div>
           </div>
           {/* After Image */}
           <div className="w-1/2 relative">
@@ -76,15 +67,12 @@ export default function ImageModal({
               className="w-full h-full object-cover aspect-[3/4]"
               alt={currentImagePair.after.title}
             />
-            <div className="absolute w-full h-full top-0 left-0 flex items-end justify-start text-white text-lg font-bold pl-6 pb-6 bg-gradient-to-t from-black/50 to-transparent">
-              AFTER
-            </div>
           </div>
           {/* Next Button */}
-          {/* {showNextButton && ( */}
+
           <button
             onClick={onNext}
-            className="absolute right-0 top-1/2 px-4 md:px-12 lg:px-4 z-10 text-white text-3xl"
+            className="absolute right-0 top-[45%] px-4 md:px-12 lg:px-4 z-10 text-white text-3xl"
             aria-label="Next image"
           >
             <FontAwesomeIcon
@@ -92,7 +80,16 @@ export default function ImageModal({
               icon={faArrowRight}
             />
           </button>
-          {/* )} */}
+        </div>
+        <div className="flex flex-col text-center text-white lg:text-lg ">
+          <div className="flex flex-row justify-center font-bold">
+            <div className="bg-main-green w-1/2 text-center py-3 lg:py-4 bg-gradient-to-l from-black/0 to-transparent">
+              <span>Before</span>
+            </div>
+            <div className="bg-main-green w-1/2 text-center py-3 lg:py-4 bg-gradient-to-l from-black/10 to-transparent">
+              <span>After</span>
+            </div>
+          </div>
         </div>
 
         <button
