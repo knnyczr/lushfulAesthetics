b4fc03a5539dd67a02df437f94b1bb9e9ec801b2
Kenneth Cruz <37004078+knnyczr@users.noreply.github.com>
2024-03-05 03:38:42 +0000

    promo banner update to copy and spacing (#126)

--- src/components/PromoBanner.js
+++ src/components/PromoBanner.js
@@ -17,7 +17,7 @@ export default function PromoBanner({ allPromosLink, promos }) {
 
   return (
     <div className="flex justify-center items-center mx-auto bg-main-green py-3 md:py-4 text-sm px-4 md:px-12 lg:px-4">
-      <div className="flex justify-between items-center w-[600px] mx-auto">
+      <div className="flex justify-between items-center w-[700px] mx-auto text-center">
         <button onClick={() => handleNavClick("left")}>
           <FontAwesomeIcon icon={faAngleLeft} />
         </button>
@@ -25,7 +25,7 @@ export default function PromoBanner({ allPromosLink, promos }) {
           <p>
             <span>{promos[currentPromotion].promoOffer} </span>
             <a href={allPromosLink}>
-              <span className="underline underline-offset-2">View all</span>
+              <span className="underline underline-offset-2">Book now</span>
             </a>
           </p>
         </div>
