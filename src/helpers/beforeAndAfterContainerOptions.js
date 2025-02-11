2176c65f5a12121ddbb8063eea68742f85d09dbf
Kenny Cruz <kennycruz@Kennys-MacBook-Pro.local>
2025-02-11 10:37:28 -0500

    adding additional text styles to before and after container

--- src/helpers/beforeAndAfterContainerOptions.js
+++ src/helpers/beforeAndAfterContainerOptions.js
@@ -3,6 +3,14 @@ import { BLOCKS, MARKS, INLINES } from "@contentful/rich-text-types";
 
 export default function beforeAndAfterContainerOptions() {
     return {
+        renderMark: {
+            [MARKS.BOLD]: (text) => (
+                <span className="font-sans font-bold mb-1">{text}</span>
+            ),
+            [MARKS.ITALIC]: (text) => (
+                <span className="font-sans italic mb-1">{text}</span>
+            ),
+        },
         renderNode: {
             [INLINES.HYPERLINK]: ({ data }, children) => (
                     <a
